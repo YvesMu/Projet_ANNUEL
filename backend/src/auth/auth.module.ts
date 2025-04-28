ea5c991ea5fa428dcfc853ceb6f670c7db6053bc
Narutino10 <ibrahim.ouahabi@outlook.fr>
2025-04-28 16:14:26 +0200

    Correction du docker pour le port

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -4,10 +4,12 @@ import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MailerModule } from '../mailer/mailer.module';
 
 @Module({
   imports: [
     UserModule,
+    MailerModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],

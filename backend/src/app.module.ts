b2a45bc5567ec4302d587ed189ed61abd8bd2828
Narutino10 <ibrahim.ouahabi@outlook.fr>
2025-04-28 15:25:31 +0200

    Correction du app.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { MailerModule } from './mailer/mailer.module';
+import { typeOrmConfig } from './typeorm.config';
 
 @Module({
-  imports: [UserModule, AuthModule, MailerModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: typeOrmConfig,
+    }),
+    UserModule,
+    AuthModule,
+    MailerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -4,12 +4,14 @@ import { JwtService } from '@nestjs/jwt';
 import bcrypt from 'bcrypt';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
+import { MailerService } from '../mailer/mailer.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly userService: UserService,
     private readonly jwtService: JwtService,
+    private readonly mailerService: MailerService,
   ) {}
 
   async register(registerDto: RegisterDto) {
@@ -24,6 +26,10 @@ export class AuthService {
     });
 
     return { message: 'Utilisateur créé avec succès', user };
+    const payload = { id: user.id, email: user.email };
+    const token = this.jwtService.sign(payload);
+
+    await this.mailerService.sendUserConfirmation(user.email, token);
   }
 
   async login(loginDto: LoginDto) {

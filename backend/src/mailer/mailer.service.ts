081c298675d9fcd8b345b37110902034ec28e7c2
Narutino10 <ibrahim.ouahabi@outlook.fr>
2025-04-28 15:21:32 +0200

    Ajout mailer

---
+++ backend/src/mailer/mailer.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { MailerService as NestMailerService } from '@nestjs-modules/mailer';
+
+@Injectable()
+export class MailerService {
+  constructor(private readonly mailerService: NestMailerService) {}
+
+  async sendUserConfirmation(email: string, token: string) {
+    const url = `http://localhost:3000/confirm-account?token=${token}`;
+
+    await this.mailerService.sendMail({
+      to: email,
+      subject: 'Confirmation de votre compte',
+      text: `Bienvenue ! Cliquez ici pour confirmer votre compte : ${url}`,
+      html: `<p>Bienvenue ! Cliquez ici pour confirmer votre compte :</p><p><a href="${url}">${url}</a></p>`,
+    });
+  }
+}

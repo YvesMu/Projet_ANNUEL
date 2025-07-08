e2104bef8475196fb11b01dbccda311f9a76fb3c
Narutino10 <ibrahim.ouahabi@outlook.fr>
2025-07-08 10:06:02 +0200

    Fix Chat and Visio

--- backend/src/video-call/video-call.module.ts
+++ backend/src/video-call/video-call.module.ts
@@ -7,9 +7,10 @@ import { User } from '../user/user.entity';
 import { Offre } from '../offre/offre.entity';
 import { VideoCallScheduler } from './video-call.scheduler';
 import { MailerModule } from '../mailer/mailer.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([VideoCall, User, Offre]), MailerModule],
+  imports: [TypeOrmModule.forFeature([VideoCall, User, Offre]), MailerModule, AuthModule],
   controllers: [VideoCallController],
   providers: [VideoCallService, VideoCallScheduler, MailerModule],
   exports: [VideoCallService],
